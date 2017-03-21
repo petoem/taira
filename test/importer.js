e4c8c7b5dcba2b6caa1f882cb870593699abbd9f
Michael Petö <michael@petoe.me>
2017-03-21 16:20:44 +0100

    Implement import for json and csv format

---
+++ test/importer.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const os = require('os');
+
+module.exports.CSV = function (path) {
+    return new Float64Array(fs.readFileSync(path, 'utf8').split(os.EOL)[1].split(';'));
+};
+
+module.exports.JSON = function (path) {
+    let data = JSON.parse(fs.readFileSync(path, 'utf8'));
+    return new Float64Array(Object.keys(data).map((val) => data[val]));
+};
