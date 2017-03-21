f87df271fb6cda4743428471417e6058f000e845
Michael Petö <michael@petoe.me>
2017-03-21 17:31:11 +0100

    Testing with random input data

--- test/test.js
+++ test/test.js
@@ -1,1 +1,9 @@
 const Taira = require('../taira.js');
+const importer = require('./importer.js');
+const exporter = require('./exporter.js');
+
+let testdata = importer.CSV(__dirname + '/data.csv');
+
+Taira.smoothen(testdata, Taira.ALGORITHMS.AVERAGE, 5, 1);
+
+exporter.CSV(__dirname + '/data_avg_5_1.csv', testdata);
