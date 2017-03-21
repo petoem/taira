3eef5d28c4dd47aac18fad61b4adf1ba10feaa03
Michael Petö <michael@petoe.me>
2017-03-21 15:36:06 +0100

    Implement data generator and exporter

---
+++ test/generate.js
@@ -0,0 +1,30 @@
+const exporter = require('./exporter.js');
+/**
+ * Test Data Generator
+ */
+
+/**
+ * Command Options
+ * ===============
+ * - first number specifies amount to generate
+ * - second number the range (0-MAX)
+ * - a string to choose export method e.g. "CSV" or "JSON"
+ */
+const genoptions = process.argv.filter((value) => !isNaN(value));
+let data = new Float64Array(genoptions[0]);
+
+data = data.map(() => {
+    return Math.random() * genoptions[1];
+});
+
+if(process.argv.includes("CSV")) {
+    exporter.CSV(__dirname + "/data.csv", data);
+}
+
+if(process.argv.includes("JSON")) {
+    exporter.JSON(__dirname + "/data.json", data);
+}
+
+if(!process.argv.includes("JSON") && !process.argv.includes("CSV")) {
+    console.log(data);
+}
