08dfb1a4d35e004fe34fd89b779f5f1e26ef980f
Michael Petö <michael@petoe.me>
2017-03-24 15:10:48 +0100

    Add simple tape test

--- test/test.js
+++ test/test.js
@@ -1,9 +1,11 @@
+const test = require('tape');
 const Taira = require('../taira.js');
 const importer = require('./importer.js');
-const exporter = require('./exporter.js');
 
-let testdata = importer.CSV(__dirname + '/data.csv');
-
-Taira.smoothen(testdata, Taira.ALGORITHMS.AVERAGE, 5, 1);
-
-exporter.CSV(__dirname + '/data_avg_5_1.csv', testdata);
+test('Taira smooth', function (t) {
+    t.plan(4);
+    t.deepLooseEqual(Taira.smoothen([], Taira.ALGORITHMS.AVERAGE, 1, 1), [], 'Empty Array');
+    t.equal(Taira.smoothen([1, 2, 3, 4, 5], Taira.ALGORITHMS.AVERAGE, 1, 1).length, 5, 'Array in/out length');
+    t.deepLooseEqual(Taira.smoothen(undefined, Taira.ALGORITHMS.AVERAGE, 1, 1), [], 'undefined outputs Array');
+    t.deepLooseEqual(Taira.from([1, 2, 3, 4, 5]), new Taira(1, 2, 3, 4, 5), 'Convert Array to Taira');
+});
