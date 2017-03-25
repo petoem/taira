90738a95d9330af2dba9b263c49501b99029849c
Michael Petö <michael@petoe.me>
2017-03-25 15:47:21 +0100

    Make array circularity optional

--- README.md
+++ README.md
@@ -8,29 +8,30 @@ Taira enables smoothing of arrays that contain numerical values, using average (
 ``` javascript
 const Taira = require('taira');
 
-let arr = [1, 10, 3, 4];
+let arr = [1, 2, 10, 4, 5, 6];
 
 /**
 * Static smooth function
-* @param {*} array The input data array (will be modified)
+* @param {*} array The input data array
 * @param {Taira.ALGORITHMS} algorithm The algorithm to use 
 * @param {integer} size How many elements before and after (e.g. size=2, means a kernel of 2*size+1)
 * @param {integer} pass How often to go over the array
-* @returns {*} Again the modified input array
+* @param {boolean} circular Joins beginning and end of array, to make the array circular
+* @returns {*} The smoothen input array
 */
-Taira.smoothen(arr, Taira.ALGORITHMS.AVERAGE, 1, 1);
+let foo = Taira.smoothen(arr, Taira.ALGORITHMS.AVERAGE, 1, 1, false);
 
-console.log(arr); // [ 5, 6, 4.333333333333333, 4.444444444444444 ]
+console.log(foo);
 
 // ... and the same for median filtering
-Taira.smoothen(arr, Taira.ALGORITHMS.MEDIAN, 2, 1);
+Taira.smoothen(arr, Taira.ALGORITHMS.MEDIAN, 2, 1, false);
 
 /*
 * ... and gaussian smoothing.
 * First integer is the size of the kernel that will be filled with values from a Gaussian distribution.
 * Last parameter is the intensity (sigma) of the distribution.
 */
-Taira.smoothen(arr, Taira.ALGORITHMS.GAUSSIAN, 2, 1);
+Taira.smoothen(arr, Taira.ALGORITHMS.GAUSSIAN, 2, 1, false);
 ```
 
 ## Extras
@@ -40,8 +41,9 @@ Taira inherits from Array, so you can use it like a normal array.
 const Taira = require('taira');
 
 let foo = new Taira(1, 2, 3, 4, 5);
-foo.smoothen(Taira.ALGORITHMS.GAUSSIAN, 2, 1.2);
-foo.push(10);
+let bar = foo.smoothen(Taira.ALGORITHMS.GAUSSIAN, 2, 1.2);
+foo.push(10); // ... lets add some more data and recalculate.
+bar = foo.smoothen(Taira.ALGORITHMS.GAUSSIAN, 2, 1.2);
 
 // ... or you could do something like this.
 let smoothsum = Taira.from([10, 20, 10, 15, 20])
--- taira.js
+++ taira.js
@@ -19,26 +19,23 @@ class Taira extends Array {
 
     /**
      * Smoothen 1D-Array using selected algorithm
-     * @param {*} array The input data array (values will be overwritten)
+     * @param {*} array The input data array
      * @param {Taira.ALGORITHMS} algorithm Takes one of the supported algorithms (defaults to AVERAGE)
      * @param {*} options Parameters for the algorithm
+     * @returns {*} New smooth array
      * @throws Will throw an error if 2*size+1>=array.length for AVERAGE,MEDIAN and GAUSSIAN algorithm
      */
     static smoothen(array, algorithm, ...options) {
-        let [option1, option2, ...other] = options;
+        let [option1, option2, option3,...other] = options;
         array = array || [];
         switch (algorithm || 0) {
             case Taira.ALGORITHMS.MEDIAN:
-                Taira[`_${Taira.ALGORITHMS.MEDIAN}`](array, option1 || 2, option2 || 1, ...other);
-                break;
+                return Taira[`_${Taira.ALGORITHMS.MEDIAN}`](array, option1 || 2, option2 || 1, ...other);
             case Taira.ALGORITHMS.GAUSSIAN:
-                Taira[`_${Taira.ALGORITHMS.GAUSSIAN}`](array, option1 || 2, option2 || 2, ...other);
-                break;
+                return Taira[`_${Taira.ALGORITHMS.GAUSSIAN}`](array, option1 || 2, option2 || 2, ...other);
             default:
-                Taira[`_${Taira.ALGORITHMS.AVERAGE}`](array, option1 || 2, option2 || 1, ...other);
-                break;
+                return Taira[`_${Taira.ALGORITHMS.AVERAGE}`](array, option1 || 2, option2 || 1, ...other);
         }
-        return array;
     }
 
     /**
@@ -46,9 +43,11 @@ class Taira extends Array {
      * @param {*} array The input data array
      * @param {integer} size The number of neighbor elements to take, results in 2*size+1
      * @param {integer} pass How many times to go over the array
+     * @returns {*} Array calculated with Taira.ALGORITHMS.AVERAGE
      */
-    static _0(array, size, pass) {
+    static _0c(array, size, pass) {
         if (array.length <= 2 * size + 1) throw new Error('Array needs to be longer than the box size (2*size+1).');
+        let out = new Array();
         array.forEach((_, index) => {
             let segmentstart = (index - size < 0) ? (index - size) + array.length : index - size;
             let sum = 0;
@@ -56,9 +55,13 @@ class Taira extends Array {
                 sum += array[a];
                 a++;
             }
-            array[index] = sum / ((size * 2) + 1);
+            out.push(sum / ((size * 2) + 1));
         });
-        if (pass > 1) Taira._0(array, size, --pass);
+        if (pass > 1) {
+            return Taira._0(array, size, --pass);
+        } else {
+            return out;
+        }
     }
 
     /**
@@ -66,9 +69,11 @@ class Taira extends Array {
      * @param {*} array The input data array
      * @param {integer} size The number of neighbor elements to take, results in 2*size+1
      * @param {integer} pass How many times to go over the array
+     * @returns {*} Array calculated with Taira.ALGORITHMS.MEDIAN
      */
-    static _1(array, size, pass) {
+    static _1c(array, size, pass) {
         if (array.length <= 2 * size + 1) throw new Error('Array needs to be longer than the box size (2*size+1).');
+        let out = new Array();
         array.forEach((_, index) => {
             let segmentstart = (index - size < 0) ? (index - size) + array.length : index - size;
             let median = new Array();
@@ -81,9 +86,13 @@ class Taira extends Array {
                 if (a > b) { return 1; }
                 return 0;
             });
-            array[index] = median[Math.trunc(((size * 2) + 1) / 2)];
+            out.push(median[Math.trunc(((size * 2) + 1) / 2)]);
         });
-        if (pass > 1) Taira._1(array, size, --pass);
+        if (pass > 1) {
+            return Taira._1(array, size, --pass);
+        } else {
+            return out;
+        }
     }
 
     /**
@@ -92,8 +101,9 @@ class Taira extends Array {
      * @param {integer} kernel Size of the kernel array is e.g. 2*kernel+1
      * @param {*} radius The blur radius (sigma from the gaussian function)
      */
-    static _2(array, kernel, radius) {
+    static _2c(array, kernel, radius) {
         if (array.length <= 2 * kernel + 1) throw new Error('Array needs to be longer than the kernel size (2*size+1).');
+        let out = new Array();
         let filter = new Float64Array(2 * kernel + 1);
         let denominator1 = radius * Math.sqrt(2 * Math.PI);
         let denominator2 = Math.pow(radius, 2) * 2;
@@ -107,8 +117,9 @@ class Taira extends Array {
             for (let a = segmentstart; (index + kernel + 1) % array.length != a; a = a % array.length) {
                 sum += array[a++] * normfilter[c++];
             }
-            array[index] = sum;
+            out.push(sum);
         });
+        return out;
     }
 }
 
