ea6999ec275ecea20e7b3ae7b1220aacdd577571
Michael Petö <michael@petoe.me>
2017-03-25 19:20:06 +0100

    Update test cases

--- test/test.js
+++ test/test.js
@@ -3,9 +3,18 @@ const Taira = require('../taira.js');
 const importer = require('./importer.js');
 
 test('Taira smooth', function (t) {
-    t.plan(4);
-    t.deepLooseEqual(Taira.smoothen([], Taira.ALGORITHMS.AVERAGE, 1, 1), [], 'Empty Array');
+    t.plan(6);
+    t.throws(() => {
+        Taira.smoothen([], Taira.ALGORITHMS.AVERAGE, 1, 1);
+        Taira.smoothen([1, 2, 4, 5], Taira.ALGORITHMS.AVERAGE, 20, 2);
+    }, 'Empty array throws');
+    t.throws(() => {
+        Taira.smoothen(undefined, Taira.ALGORITHMS.AVERAGE, 1, 1);
+    }, 'undefined throws');
+    t.throws(() => {
+        Taira.smoothen([1, 2, 4, 5], Taira.ALGORITHMS.AVERAGE, 20, 2);
+    }, 'Too short array for size=20 throws');
     t.equal(Taira.smoothen([1, 2, 3, 4, 5], Taira.ALGORITHMS.AVERAGE, 1, 1).length, 5, 'Array in/out length');
-    t.deepLooseEqual(Taira.smoothen(undefined, Taira.ALGORITHMS.AVERAGE, 1, 1), [], 'undefined outputs Array');
     t.deepLooseEqual(Taira.from([1, 2, 3, 4, 5]), new Taira(1, 2, 3, 4, 5), 'Convert Array to Taira');
+    t.deepLooseEqual(Taira.smoothen([1, 2, 3, 4, 5], Taira.ALGORITHMS.AVERAGE, 1, 1), [ 2.6666666666666665, 2, 3, 4, 3.3333333333333335 ], 'Expected output');
 });
